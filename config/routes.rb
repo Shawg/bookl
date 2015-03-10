1c55a02e3f37a0d499f836f3e01f31db10fa521e
Eli <eliasjf@britecontent.com>
2015-03-10 14:15:12 -0700

    Add about page and change home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :user_accounts
   get 'user_accounts/index'
 
+  get 'static_pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
