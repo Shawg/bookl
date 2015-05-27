2b5487150028da8fbfd7c7db52b50ccd4ca10983
Eli <eliasjf@britecontent.com>
2015-05-26 20:40:47 -0600

    refactored admin view for recent db refactor added routs for user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  get 'dashboard' => 'user_accounts#dashboard'
   
   resources :user_accounts
   resources :posts
